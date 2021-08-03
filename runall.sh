6664b30f97eb2f98a02ee2161f8ea236febb5d12
ryu10 <ryu10nt@gmail.com>
2021-08-03 14:45:02 -0700

    update runall.sh to add slot/drive info

--- runall.sh
+++ runall.sh
@@ -2,17 +2,15 @@
 make all
 make disk
 
-echo "CATALOG,S2,D2" | a2term 
-
 cd basicHello
 make mount
-echo "RUN HELLOTEST" | a2term
-sleep 5
+echo "RUN HELLOTEST,S2,D2" | a2term
+sleep 4
 
 cd ../cHello
 make mount
 echo "BRUN CHELLO" | a2term
-sleep 1
+sleep 2
 
 cd ../asmHello
 make mount
